be8f861d432d51647e06f387f2c44f4d2deaeca9
Chris-Delfaus <ChrisDelf@protonmail.com>
2019-12-02 13:57:42 -0500

    completed all the way to part 4

--- src/03_modules.py
+++ src/03_modules.py
@@ -10,12 +10,15 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+for argu in sys.argv:
+    print('>> Arguments: ', argu)
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
-
+print('>> OS: ', sys.platform)
 # Print out the version of Python you're using:
 # YOUR CODE HERE
+print('>> Python Version: ', sys.version_info)
 
 
 import os
